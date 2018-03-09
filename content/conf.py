86448790d3ff37c4ea2b75c98430e2fbe3a14382
Jesse Griffin <jag3773@gmail.com>
2018-03-09 20:33:21 +0000

    Set SBL Hebrew as preferred font for Hebrew text (#328)

--- content/conf.py
+++ content/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'unfoldingWord Hebrew Grammar'
-copyright = u'2018, unfoldingWord'
+copyright = u'2018, unfoldingWord, CC BY-SA 4.0'
 author = u'unfoldingWord'
 
 # The version info for the project you're documenting, acts as replacement for
